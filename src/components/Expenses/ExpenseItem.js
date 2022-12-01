0de14c6219fa9de5446b37328360ce8f0f23587a
MaheshKumarPratihar <sam43592@gmail.com>
2022-12-01 12:14:22 +0530

    final

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -22,7 +22,7 @@ const ExpenseItem = (props) => {
           <h2>{title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change title</button>
+        {/* <button onClick={clickHandler}>Change title</button> */}
       </div>
     </ul>
   );
